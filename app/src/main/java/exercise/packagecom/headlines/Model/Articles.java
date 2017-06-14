8e12b7b61cb6428d68423beadf943b7c371b96fa
abhijeet-15 <abhijeet.hcs@gmail.com>
2017-06-14 14:19:05 +0530

    First commit

---
+++ app/src/main/java/exercise/packagecom/headlines/Model/Articles.java
@@ -0,0 +1,30 @@
+package exercise.packagecom.headlines.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by abhijeet on 8/6/17.
+ */
+
+public class Articles {
+
+    @SerializedName("title")
+    @Expose
+
+    private String title;
+
+    public String getTile(){
+        return title;
+    }
+
+    @SerializedName("urlToImage")
+    @Expose
+
+    private String url;
+
+    public String getUrl(){
+        return url;
+    }
+
+}
